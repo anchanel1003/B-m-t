3090a3c6ad9790719890ff7ea93ca0904fe5842f
annv <annv@hurasoft.com>
2023-03-08 00:26:55 +0700

    update

---
+++ config.js
@@ -0,0 +1,10 @@
+const nameGirl = 'Mai';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Chúc Mừng 8-3';
+const titleCard = 'Tặng Mai ';
+const contentCard = 'Chúc Mai 8-3 tràn ngập niềm vui và những nụ cười. Mong điều tốt đẹp nhất sẽ đến với Mai trong hôm nay và cả những ngày sau';
+
+
+const giftImage = 'hot-girl.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
